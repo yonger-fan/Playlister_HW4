a73f998bcb16ac0c3250b56eca792f167d152b8f
Yonger <yongerfan12@gmail.com>
2022-11-02 15:59:12 -0400

    User Login

--- client/src/auth/index.js
+++ client/src/auth/index.js
@@ -78,7 +78,7 @@ function AuthContextProvider(props) {
                     user: response.data.user
                 }
             })
-            history.push("/login");
+            history.push("/");
         }
     }
 
